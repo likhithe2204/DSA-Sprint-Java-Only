d1e16b116098f6675e7eb6276bfabc0c602b942a
Likhith Edupuganti <159748439+likhithe2204@users.noreply.github.com>
2025-10-02 20:42:31 +0530

    Update RecursiveInorderTraversal.java

--- Binary Trees/RecursiveInorderTraversal.java
+++ Binary Trees/RecursiveInorderTraversal.java
@@ -9,7 +9,7 @@ Given the root of a binary tree, return the inorder traversal of its nodes' valu
 🎯 Input/Output:
 Example 1:  
 Input: root = [1,null,2,3]  
-Output: [1,3,2]  
+Output: [1,3,2]   
 
 Example 2:  
 Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]  
